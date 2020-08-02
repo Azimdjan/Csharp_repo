85bc2fc4452e98731dc3587d3e3bc1233b28c2f8
Azimdjan <68198468+Azimdjan@users.noreply.github.com>
2020-08-02 21:31:18 +0500

    Update for project 1

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -4,9 +4,15 @@ namespace Project1
 {
     class Program
     {
+        /// <summary>
+        /// Creating variables for learning purposes
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = 19;
+            Console.WriteLine("My age is: " + age);
+            Console.WriteLine();
         }
     }
 }
