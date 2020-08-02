6ecd7901b01ac45bd0e519557b1ae2ca5617224c
Azimdjan <68198468+Azimdjan@users.noreply.github.com>
2020-08-02 23:41:48 +0500

    New third project was added

---
+++ Project3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter degree: ");
+            float degree = float.Parse(Console.ReadLine());
+            float cosine = (float)Math.Cos(degree * Math.PI / 180);
+            Console.WriteLine("Cosine of " + degree + " is " + cosine);
+            float sinus = (float)Math.Sin(degree * Math.PI / 180);
+            Console.WriteLine("Sinus of " + degree + " is " + sinus);
+            Console.WriteLine();
+        }
+    }
+}
