6c1090846b1bd81142442d55b0a8d472111b886b
Azimdjan <68198468+Azimdjan@users.noreply.github.com>
2020-08-02 21:48:26 +0500

    Project 2 updated

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -6,7 +6,21 @@ namespace Project2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter first location: ");
+            int firstAltitude = int.Parse(Console.ReadLine());
+            Console.Write("Please enter second location: ");
+            int secondAltitude = int.Parse(Console.ReadLine());
+            int difference = 0;
+            if(firstAltitude>secondAltitude)
+            {
+                difference = firstAltitude - secondAltitude;
+            }
+            else
+            {
+                difference = secondAltitude - firstAltitude;
+            }
+            Console.WriteLine("Difference between locations is: " + difference + " km");
+            Console.WriteLine();
         }
     }
 }
