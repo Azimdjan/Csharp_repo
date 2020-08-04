0b494e0b6a544e10f14d1cd37444920e9f3a6b64
Azimdjan <68198468+Azimdjan@users.noreply.github.com>
2020-08-04 22:24:38 +0500

    Project 4 updated

--- Project4/Exercise8/Exercise8/Program.cs
+++ Project4/Exercise8/Exercise8/Program.cs
@@ -21,11 +21,19 @@ namespace Exercise8
             RandomNumberGenerator.Initialize();
 
             // create two dice
+            Die die1 = new Die(6);
+            Die die2 = new Die(6);
 
             // tell the dice to roll themselves
+            die1.Roll();
+            die2.Roll();
+
 
             // print the top sides and the sum of the dice
-
+            Console.WriteLine("Top side of die 1 is: " + die1.TopSide);
+            Console.WriteLine("Top side of die 2 is: " + die2.TopSide);
+            int sumOfDies = die1.TopSide + die2.TopSide;
+            Console.WriteLine("Sum of top sides is: " + sumOfDies);
             Console.WriteLine();
         }
     }
