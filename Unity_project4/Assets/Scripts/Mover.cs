7ac9c92032b8c96d5ed541ff73f1311ed4f7e2ac
Azimdjan <68198468+Azimdjan@users.noreply.github.com>
2020-08-06 22:52:01 +0500

    Unity project4 updated

---
+++ Unity_project4/Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    Rigidbody2D myRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        Vector2 speed = new Vector2(1, 2);
+        myRigidbody.AddForce(speed, ForceMode2D.Impulse);
+    }
+}
