ad9b83096e6c96f667bacbab82e64bb47cde4c2a
Azimdjan <68198468+Azimdjan@users.noreply.github.com>
2020-08-09 15:42:19 +0500

    Unity project 7 was created

---
+++ Unity_project7/Assets/Scripts/Resizer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Resizer : MonoBehaviour
+{
+    const float TotalTimeElapsed = 3f;
+    float elapsedTime = 0f;
+    const float ScaleFactor = 2f;
+    int ScaleSignFactor = 1;
+
+    private void Timer()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= TotalTimeElapsed)
+        {
+            elapsedTime = 0;
+            ScaleSignFactor *= -1;
+        }
+    }
+
+    private void Resize()
+    {
+        Vector2 resize = transform.localScale;
+        resize.x += Time.deltaTime*ScaleFactor*ScaleSignFactor;
+        resize.y += Time.deltaTime*ScaleFactor * ScaleSignFactor;
+        transform.localScale = resize;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+        Resize();
+    }
+}
