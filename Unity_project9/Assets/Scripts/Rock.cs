dd476aa1d239cca8136a1b8480f9a87c97ae99ac
Azimdjan <68198468+Azimdjan@users.noreply.github.com>
2020-08-15 23:15:27 +0500

    unity project 9 was created and ended

---
+++ Unity_project9/Assets/Scripts/Rock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    Rigidbody2D myrigidBody;
+    const float MinSpeed = 1f;
+    const float MaxSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myrigidBody = GetComponent<Rigidbody2D>();
+        float angle = Random.Range(0.2f, Mathf.PI);
+        float magnitude = Random.Range(MinSpeed, MaxSpeed);
+        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        myrigidBody.AddForce(direction * magnitude, ForceMode2D.Impulse);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Unity_project9/Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject rockPrefab;
+    [SerializeField] Sprite[] sprites;
+    private SpriteRenderer sprite;
+    private GameObject[] rocks;
+    [SerializeField] int numberRocks;
+    private bool isSpawning = false;
+    Timer spawnTimer;
+    const float spawnTime = 1f;
+    private float spawnLocationX;
+    private float spawnLocationY;
+    private int option = 3;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        spawnTimer = gameObject.AddComponent<Timer>();
+    }
+
+    void Start()
+    {
+        Instantiating();
+        SettingTimer();
+        CreatingLocation();
+    }
+
+    private void SettingTimer()
+    {
+        spawnTimer.Duration = spawnTime;
+        spawnTimer.Run();
+    }
+
+    private void Instantiating()
+    {
+        rocks = GameObject.FindGameObjectsWithTag("Rock");
+        numberRocks = rocks.Length;
+    }
+
+    private void CreatingLocation()
+    {
+        spawnLocationX = Screen.width / 2;
+        spawnLocationY = Screen.height / 2;
+    }
+
+    private void SpawningRocks()
+    {
+        Vector2 location = new Vector2(spawnLocationX, spawnLocationY);
+        Vector2 worldLocation = Camera.main.ScreenToWorldPoint(location);
+        ChangingSprite();
+        GameObject currentRock = Instantiate<GameObject>(rockPrefab) as GameObject;
+        currentRock.transform.position = worldLocation;
+    }
+
+    private void ChangingSprite()
+    {
+        int currentSprite = Random.Range(0, option);
+        rockPrefab.GetComponent<SpriteRenderer>().sprite = sprites[currentSprite];
+    }
+
+    private void SpawningWithDelay()
+    {
+        if(spawnTimer.Finished&&numberRocks<3)
+        {
+            SpawningRocks();
+            spawnTimer.Duration = spawnTime;
+            spawnTimer.Run();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiating();
+        SpawningWithDelay();
+    }
+}
