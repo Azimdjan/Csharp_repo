96b61f52499892202675f24788019ed3fdb11c85
Azimdjan <68198468+Azimdjan@users.noreply.github.com>
2020-08-18 00:45:25 +0500

    unity project 11 was updated

--- Unity_project11/Assets/scripts/MouseButtonProcessor.cs
+++ Unity_project11/Assets/scripts/MouseButtonProcessor.cs
@@ -16,14 +16,57 @@ public class MouseButtonProcessor : MonoBehaviour
     bool spawnInputOnPreviousFrame = false;
 	bool explodeInputOnPreviousFrame = false;
 
-	/// <summary>
-	/// Update is called once per frame
-	/// </summary>
+
+    private Vector2 FindMousePosition
+    {
+        get
+        {
+            Vector2 mousePositionScreen = Input.mousePosition;
+            Vector2 mousePositionWorld = Camera.main.ScreenToWorldPoint(mousePositionScreen);
+            return mousePositionWorld; 
+        }
+    }
+    private void SpawnTeddyBears()
+    {
+        if(Input.GetAxis("SpawnTeddyBears")>0)
+        {
+            if(!spawnInputOnPreviousFrame)
+            {
+                spawnInputOnPreviousFrame = true;
+                GameObject currentTeddy = Instantiate<GameObject>(prefabTeddyBear) as GameObject;
+                currentTeddy.transform.position = FindMousePosition;
+            }
+        }
+        else
+        {
+            spawnInputOnPreviousFrame = false;
+        }
+    }
+
+    private void ExplodeTeddyBears()
+    {
+        GameObject randomTeddy = GameObject.FindWithTag("TeddyBear");
+        if(randomTeddy&&Input.GetAxis("ExplodeTeddyBears") >0)
+        {
+            if (!explodeInputOnPreviousFrame)
+            {
+                explodeInputOnPreviousFrame = true;
+                GameObject explosion = Instantiate<GameObject>(prefabExplosion) as GameObject;
+                explosion.transform.position = randomTeddy.transform.position;
+                Destroy(randomTeddy);
+            }
+        }
+        else
+        {
+            explodeInputOnPreviousFrame = false;
+            Debug.LogError("Do not have any teddy bears!");
+        }
+    }
 	void Update()
 	{
         // spawn teddy bear as appropriate
-
+        SpawnTeddyBears();
         // explode teddy bear as appropriate
-		
+        ExplodeTeddyBears();
 	}
 }
