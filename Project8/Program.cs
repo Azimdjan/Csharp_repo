42cb2eeeed12fab8e2c15afaf856e4fe84ba21e1
Azimdjan <68198468+Azimdjan@users.noreply.github.com>
2020-08-23 23:16:49 +0500

    project 8 and project 9 was created

---
+++ Project8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Add(i+1);
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine();
+
+            for(int i=numbers.Count;i>0;i--)
+            {
+                if (i % 2 == 0)
+                    numbers.Remove(i);
+            }
+
+            for (int i= 0;i < numbers.Count;i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ Project9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleCards;
+
+namespace Project9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            List<Card> hand = new List<Card>();
+            deck.Shuffle();
+            for(int i=0;i<5;i++)
+            {
+                hand.Add(deck.TakeTopCard());
+            }
+            foreach(Card card in hand)
+            {
+                card.FlipOver();
+                card.Print();
+            }
+            Console.WriteLine();
+        }
+    }
+}
